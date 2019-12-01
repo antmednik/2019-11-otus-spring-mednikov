6dd04a209a8607c3b2a9b7a47266364200d650b9
anton.mednikov <antmednik@gmail.com>
2019-12-01 03:22:36 +0300

    hw-01: final fixes

--- homework-01/src/main/java/ru/otus/homework/hw01/Hw01Application.java
+++ homework-01/src/main/java/ru/otus/homework/hw01/Hw01Application.java
@@ -6,12 +6,8 @@ import ru.otus.homework.hw01.service.test.ui.ConsoleTestUI;
 public class Hw01Application {
 
 	public static void main(String[] args) {
-
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
 		ConsoleTestUI consoleUI = context.getBean(ConsoleTestUI.class);
 		consoleUI.runTest();
-		/*PersonService service = context.getBean(PersonService.class);
-		Person ivan = service.getByName("Ivan");
-		System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());*/
 	}
 }
