2a960af6f91525a6f10adc3ac3ff8d2e7633023f
anton.mednikov <antmednik@gmail.com>
2020-03-22 12:14:18 +0300

    hw-28

---
+++ homework-28/src/test/java/ru/otus/homework/hw28/Hw28ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.homework.hw28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
